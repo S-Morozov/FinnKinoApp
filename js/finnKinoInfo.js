7d8d490a8eb8c188cedf92943ce6a15309318e76
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-06-01 08:27:53 +0300

    #changes in index and css

--- js/finnKinoInfo.js
+++ js/finnKinoInfo.js
@@ -9,70 +9,57 @@ const defaultValues = document.getElementById('film-table');
 
 currentdata.loadDoc();
 
-setTimeout(dropdowndata,1000)
+setTimeout(dropdowndata, 1000);
 
-
-function dropdowndata(){
-
-    setTimeout(function (){
-    for (let i = 1; i < currentdata.Filmkuvalista.length; i++) {
-        movielist = currentdata.Filmkuvalista;
-    }
-    console.log("Toimii")
-    let result = movielist.filter(
-        (item, index) => index === movielist.findIndex(
-            other => item.cinemaName === other.cinemaName
-        ));
-    let mySelect = document.getElementById('custom-select')
-    for (let j = 1; j<result.length; j++) {
-        let resultNew = result.sort();
-
-
-        let myOption = document.createElement('option')
-        myOption.text = resultNew[j].cinemaName;
-        myOption.value = j;
-        mySelect.appendChild(myOption)
-
-    }
-
-},500);
-
+async function dropdowndata() {
+    setTimeout(async () => {
+        movielist = currentdata.Filmkuvalista.slice(1);
+        console.log("Toimii");
+        let result = movielist.filter((item, index) => index === movielist.findIndex(other => item.cinemaName === other.cinemaName));
+        let mySelect = document.getElementById('custom-select');
+        for (let j = 1; j < result.length; j++) {
+            let resultNew = result.sort();
+            let myOption = document.createElement('option');
+            myOption.text = resultNew[j].cinemaName;
+            myOption.value = j;
+            mySelect.appendChild(myOption);
+        }
+    }, 500);
 }
 
 function finnKinoSheudule() {
-
-    console.log("SISÄLLÄ")
-
+    console.log("SISÄLLÄ");
     for (let i = 1; i < movielist.length; i++) {
         let hours = Math.floor(movielist[i].movieduration / 60);
         let minutes = movielist[i].movieduration % 60;
         let movieTimeConvert = hours + 't ' + minutes + ' min';
-        let table;
-        table = '<tr>' +
-            '<td class="poster"><img src="' + movielist[i].srcS + '" alt="image"></td>' +
-            '<td class="film-title">' + movielist[i].name +
-            '</td>' + '<br>' +
-            '<td>' + movielist[i].movieGenre + '<br>' +
-            '<p>' + movieTimeConvert + '</p>' +
-            '<p>Rating:' + movielist[i].movieRating + '</p>' +
-            '</td>' +
-            '<td><a href=" ' + movielist[i].movieurl + ' "><button type="submit" class="btn-ticket"' +
-            ' style="width: 100px; height: 50px; background-color: crimson;' +
-            'color: white; border-radius: 10px; ">Osta liput</button></a></td>' +
-            '<td>' + movielist[i].cinemaName + '</td>' +
-            '</tr>';
+        let table = `
+      <tr>
+        <td class="poster"><img src="${movielist[i].srcS}" alt="image"></td>
+        <td class="film-title">${movielist[i].name}</td>
+        <td>
+          ${movielist[i].movieGenre}<br>
+          <p>${movieTimeConvert}</p>
+          <p>Rating: ${movielist[i].movieRating}</p>
+        </td>
+        <td>
+          <a href="${movielist[i].movieurl}">
+            <button type="submit" class="btn-ticket"
+              style="width: 100px; height: 50px; background-color: crimson;
+              color: white; border-radius: 10px;">Osta liput</button>
+          </a>
+        </td>
+        <td>${movielist[i].cinemaName}</td>
+      </tr>
+    `;
         document.getElementById("film-table").innerHTML += table;
     }
 }
 
-
-// tämä funktio vaihtaa dropdown value
 function changeVal() {
-
     let index = document.getElementById('custom-select').selectedIndex;
     let Tchoose = movielist[index];
-    let thechosenvalue = document.getElementById('custom-select').options[index].text
-
+    let thechosenvalue = document.getElementById('custom-select').options[index].text;
 
     for (let i = 0; i < movielist.length; i++) {
         let hours = Math.floor(movielist[i].movieduration / 60);
@@ -80,23 +67,22 @@ function changeVal() {
         let movieTimeConvert = hours + ' t ' + minutes + ' min';
         let table;
         if (movielist[i].cinemaName === thechosenvalue) {
-            console.log("YES")
-            table = '<tr>' +
-                '<td class="poster"><img  class = "tableimage" src="' + movielist[i].srcS + '" alt="image"></td>' +
-                '<td class="film-title">' + movielist[i].name +
-                '</td>' + '<br>' +
-                '<td>' + movielist[i].movieGenre + '<br>' +
-                '<p>' + movieTimeConvert + '</p>' +
-                '<p>Rating: ' + movielist[i].movieRating + '</p>' +
-                '</td>' +
-                '<td><a href=" ' + movielist[i].movieurl + ' "><button type="submit" class="btn-ticket">Osta liput</button></a></td>' +
-                '<td>' + movielist[i].cinemaName + '</td>' +
-                '</tr>';
-
-
+            console.log("YES");
+            table = `
+        <tr>
+          <td class="poster"><img class="tableimage" src="${movielist[i].srcS}" alt="image"></td>
+          <td class="film-title">${movielist[i].name}</td>
+          <td>
+            ${movielist[i].movieGenre}<br>
+            <p>${movieTimeConvert}</p>
+            <p>Rating: ${movielist[i].movieRating}</p>
+          </td>
+          <td><a href="${movielist[i].movieurl}"><button type="submit" class="btn-ticket">Osta liput</button></a></td>
+          <td>${movielist[i].cinemaName}</td>
+        </tr>
+      `;
             document.getElementById("film-table").innerHTML += table;
         }
-
     }
 }
 
@@ -104,11 +90,11 @@ button.addEventListener('click', changeVal);
 dropdown.addEventListener('change', resetInputs);
 
 function resetInputs() {
-    // Reset the child select options to default
-    if (counter === 1 ) {
+    if (counter === 1) {
         counter = 0;
     }
     defaultValues.innerHTML = '';
 }
+
 
 
